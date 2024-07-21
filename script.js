4ce674583966e92ac10374e9e0ff7fcf499d46b4
PUSTAKA-JAYA <novanategar@gmail.com>
2024-07-22 00:30:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     $("#flipbook").turn({
-        width: 600,
-        height: 400,
+        width: $('#flipbook').width(),
+        height: $('#flipbook').height(),
         autoCenter: true,
-        display: 'single'  // Tambahkan ini untuk menampilkan satu halaman
+        display: 'single'  // Menampilkan satu halaman
+    });
+
+    // Fungsi untuk menyesuaikan ukuran flipbook saat ukuran jendela berubah
+    $(window).resize(function() {
+        $("#flipbook").turn("size", $('#flipbook').width(), $('#flipbook').height());
     });
 });
