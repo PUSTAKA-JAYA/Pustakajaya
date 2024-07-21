8ad06622af3e90fc9608d8567637d332d5603000
PUSTAKA-JAYA <novanategar@gmail.com>
2024-07-21 22:59:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let currentPage = 1;
-    const pages = document.querySelectorAll('.page');
-    const totalPages = pages.length;
-
-    document.addEventListener('click', () => {
-        pages.forEach((page, index) => {
-            page.style.transform = `rotateY(${(index - currentPage) * 180}deg)`;
-        });
-
-        currentPage = (currentPage + 1) % totalPages;
+    $("#flipbook").turn({
+        width: 600,
+        height: 400,
+        autoCenter: true
     });
 });
